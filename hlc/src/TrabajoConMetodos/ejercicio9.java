f71ed7d045761cb5527a81db168b8e98dd1df177
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-10-19 09:21:09 +0200

    qa

--- hlc/src/TrabajoConMetodos/ejercicio9.java
+++ hlc/src/TrabajoConMetodos/ejercicio9.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ejercicio9 {
     public static void main(String[] args) {
-        //aaa
+        
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("introduce un numero:");
+        int num=sc.nextInt();
+
+        System.out.println("Introduce un exponente:");
+        int exponente=sc.nextInt();
+
+        
+
+        System.out.println("Resultado: "+BaseExponente(num, exponente));
+
+        sc.close();
     }
+
+    private static int BaseExponente(int num, int exponente) {
+        for (int i = 0; i < exponente; i++) {
+            num*=num;
+        }
+
+        return num;
+    }
+
 }
